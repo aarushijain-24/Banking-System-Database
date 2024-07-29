8656c19ad0bc6313e104c4d2c7169da7ebfcefd1
Aarushi Jain <144267641+aarushijain-24@users.noreply.github.com>
2024-07-29 12:28:50 +0530

    Update Procedure_OpenAccount.sql

--- Procedure_OpenAccount.sql
+++ Procedure_OpenAccount.sql
@@ -1,9 +1,10 @@
-CREATE PROCEDURE OpenAccount
+CREATE OR ALTER PROCEDURE OpenAccount
+	@AccountNumber INT,
     @CustomerID INT,
     @AccountType NVARCHAR(50),
     @InitialDeposit DECIMAL(18, 2)
 AS
 BEGIN
-    INSERT INTO Accounts (CustomerID, AccountType, Balance)
-    VALUES (@CustomerID, @AccountType, @InitialDeposit);
+    INSERT INTO Accounts (AccountNumber, CustomerID, AccountType, Balance)
+    VALUES (@AccountNumber, @CustomerID, @AccountType, @InitialDeposit);
 END;
