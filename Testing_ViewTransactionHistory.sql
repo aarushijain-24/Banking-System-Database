4d336a50398bb8300d1ad07b1025699b81ece9a9
Aarushi Jain <144267641+aarushijain-24@users.noreply.github.com>
2024-07-30 09:11:08 +0530

    Create Testing_ViewTransactionHistory.sql

---
+++ Testing_ViewTransactionHistory.sql
@@ -0,0 +1,5 @@
+-- Viewing Transaction History
+-- Ensure transactions are listed in descending order of date.
+-- Verify all transaction details are correct.
+EXEC ViewTransactionHistory @AccountNumber = 100001;
+EXEC ViewTransactionHistory @AccountNumber = 100004;
